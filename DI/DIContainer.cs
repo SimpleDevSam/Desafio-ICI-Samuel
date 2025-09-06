392140e0c2f9a9b5d0e5425a491ba0e6895f9bd2
Samuel Maia <samuel.assis.ext@acelen.com>
2025-09-06 12:33:11 -0300

    adicionando container de Di para isolar código e limpar o program.cs

---
+++ DI/DIContainer.cs
@@ -0,0 +1,51 @@
+﻿using Desafio_ICI_Samuel.Features.News.Delete;
+using Desafio_ICI_Samuel.Features.News.Edit;
+using Desafio_ICI_Samuel.Features.News.Form;
+using Desafio_ICI_Samuel.Features.News.Get;
+using Desafio_ICI_Samuel.Features.News.ListNews;
+using Desafio_ICI_Samuel.Features.Tags.Create;
+using Desafio_ICI_Samuel.Features.Tags.Delete;
+using Desafio_ICI_Samuel.Features.Tags.Edit;
+using Desafio_ICI_Samuel.Features.Tags.Get;
+using Desafio_ICI_Samuel.Features.Tags.List;
+using Desafio_ICI_Samuel.Features;
+using Desafio_ICI_Samuel.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Desafio_ICI;
+
+public static class DependencyInjection
+{
+
+    public static IServiceCollection AddHandlers(this IServiceCollection services)
+    {
+        services.AddScoped<ICreateTagHandler, CreateTagHandler>();
+        services.AddScoped<IListTagsHandler, ListTagsHandler>();
+        services.AddScoped<IEditTagHandler, EditTagHandler>();
+        services.AddScoped<IDeleteTagHandler, DeleteTagHandler>();
+        services.AddScoped<IGetTagHandler, GetTagHandler>();
+        services.AddScoped<ICreateNewsHandler, CreateNewsHandler>();
+        services.AddScoped<IEditNewsHandler, EditNewsHandler>();
+        services.AddScoped<IDeleteNewsHandler, DeleteNewsHandler>();
+        services.AddScoped<IGetNewsHandler, GetNewsHandler>();
+        services.AddScoped<IListNewsHandler, ListNewsHandler>();
+        services.AddScoped<IFormBuilder, FormBuilder>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddPersistence(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        var env = builder.Environment;
+
+        var dbDir = Path.Combine(env.ContentRootPath, "Data", "DB");
+        Directory.CreateDirectory(dbDir);
+        var dbPath = Path.Combine(dbDir, "ici.db");
+
+        services.AddDbContext<AppDbContext>(opt =>
+            opt.UseSqlite($"Data Source={dbPath};Cache=Shared"));
+
+        return services;
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,54 +1,17 @@
-using Desafio_ICI_Samuel.Data;
-using Desafio_ICI_Samuel.Features;
-using Desafio_ICI_Samuel.Features.News.Delete;
-using Desafio_ICI_Samuel.Features.News.Edit;
-using Desafio_ICI_Samuel.Features.News.Form;
-using Desafio_ICI_Samuel.Features.News.Get;
-using Desafio_ICI_Samuel.Features.News.ListNews;
-using Desafio_ICI_Samuel.Features.Tags.Create;
-using Desafio_ICI_Samuel.Features.Tags.Delete;
-using Desafio_ICI_Samuel.Features.Tags.Edit;
-using Desafio_ICI_Samuel.Features.Tags.Get;
-using Desafio_ICI_Samuel.Features.Tags.List;
-using Microsoft.EntityFrameworkCore;
+using Desafio_ICI;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
-
-var dbPath = Path.Combine(builder.Environment.ContentRootPath, "Data/DB", "ici.db");
-Directory.CreateDirectory(Path.GetDirectoryName(dbPath)!);
-builder.Services.AddDbContext<AppDbContext>(opt =>opt.UseSqlite($"Data Source={dbPath};Cache=Shared"));
-
-builder.Services.AddScoped<ICreateTagHandler,CreateTagHandler>();
-builder.Services.AddScoped<IListTagsHandler, ListTagsHandler>();
-builder.Services.AddScoped<IEditTagHandler,EditTagHandler>();
-builder.Services.AddScoped<IDeleteTagHandler,DeleteTagHandler>();
-builder.Services.AddScoped<IGetTagHandler,GetTagHandler>();
-builder.Services.AddScoped<ICreateNewsHandler, CreateNewsHandler>();
-builder.Services.AddScoped<IEditNewsHandler, EditNewsHandler>();
-builder.Services.AddScoped<IDeleteNewsHandler, DeleteNewsHandler>();
-builder.Services.AddScoped<IGetNewsHandler, GetNewsHandler>();
-builder.Services.AddScoped<IListNewsHandler, ListNewsHandler>();
-builder.Services.AddScoped<IFormBuilder, FormBuilder>();
+builder.Services.AddPersistence(builder);
+builder.Services.AddHandlers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
-
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    app.Logger.LogInformation("SQLite file => {Path}",
-        Path.GetFullPath(db.Database.GetDbConnection().DataSource ?? "unknown"));
-    await db.Database.MigrateAsync();
 }
 
 app.UseHttpsRedirection();
